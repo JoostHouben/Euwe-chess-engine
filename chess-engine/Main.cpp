9ebb6ee4a7823e186dc946e1074af72e5e6eb2b9
Joost Houben <j.b.houben@gmail.com>
2024-05-18 17:20:54 -0700

    Reduce bishop value to that of knight and add bishop pair bonus.
    Testing shows inconclusive results

--- chess-engine/Main.cpp
+++ chess-engine/Main.cpp
@@ -120,7 +120,7 @@ void handleGo(std::stringstream& lineSStream, UciState& uciState) {
 }
 
 void runUci() {
-    std::print("id name bishop-eval\n");
+    std::print("id name bishop-pair\n");
     std::print("id author Joost Houben\n");
     std::print("uciok\n");
 
