0260fe3f5a40a42b4517bf9a12bab977789a40f8
Joost Houben <j.b.houben@gmail.com>
2024-09-26 22:29:02 -0700

    Retune with even more data.

--- chess-engine-lib/UciFrontEnd.cpp
+++ chess-engine-lib/UciFrontEnd.cpp
@@ -141,7 +141,7 @@ UciFrontEnd::Impl::~Impl() {
 }
 
 void UciFrontEnd::Impl::run() {
-    writeUci("id name retune-attack-defend");
+    writeUci("id name retune2-attack-defend");
     writeUci("id author Joost Houben");
 
     writeOptions();
--- tuner/Main.cpp
+++ tuner/Main.cpp
@@ -44,9 +44,10 @@ int main(int argc, char** argv) {
             R"(D:\annotated-fens\first-fine-tuning.txt)",
             R"(D:\annotated-fens\more-tapered-eval-terms-tuning.txt)",
             R"(D:\annotated-fens\bad-castling-bonus-tune.txt)",
-            R"(D:\annotated-fens\attack-defend.txt)"};
+            R"(D:\annotated-fens\attack-defend.txt)",
+            R"(D:\annotated-fens\global-pawn-adjustment.txt)"};
 
-    const std::vector<int> dropoutRates = {8, 2, 4, 2, 2, 1};
+    const std::vector<int> dropoutRates = {8, 2, 4, 2, 2, 2, 1};
 
     std::println("Loading positions...");
     std::vector<ScoredPosition> scoredPositions;
