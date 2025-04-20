130df5735e53efd46adaf8a60d42e4e89e5e323f
Joost Houben <j.b.houben@gmail.com>
2025-04-20 16:41:44 -0700

    Add valgrind test run to CI

--- tests/HashingTests.cpp
+++ tests/HashingTests.cpp
@@ -181,15 +181,24 @@ std::string hashTestName(const ::testing::TestParamInfo<HashCollisionTestConfig>
     return fenName + "_depth" + std::to_string(info.param.depth);
 }
 
-const auto testCases = ::testing::Values(
-        HashCollisionTestConfig{.fen = getStartingPositionFen(), .depth = 4},
+const auto testCasesFast = ::testing::Values(
+        HashCollisionTestConfig{.fen = getStartingPositionFen(), .depth = 3},
         HashCollisionTestConfig{.fen = kKiwipeteFen, .depth = 3},
-        HashCollisionTestConfig{.fen = kPosition3Fen, .depth = 5},
-        HashCollisionTestConfig{.fen = kPosition4Fen, .depth = 4},
+        HashCollisionTestConfig{.fen = kPosition3Fen, .depth = 4},
+        HashCollisionTestConfig{.fen = kPosition4Fen, .depth = 3},
         HashCollisionTestConfig{.fen = kPosition5Fen, .depth = 3},
         HashCollisionTestConfig{.fen = kPosition6Fen, .depth = 3});
 
-INSTANTIATE_TEST_SUITE_P(HashCollisionTests, HashCollisionTests, testCases, hashTestName);
+const auto testCasesSlow = ::testing::Values(
+        HashCollisionTestConfig{.fen = getStartingPositionFen(), .depth = 4},
+        HashCollisionTestConfig{.fen = kPosition3Fen, .depth = 5},
+        HashCollisionTestConfig{.fen = kPosition4Fen, .depth = 4});
+
+INSTANTIATE_TEST_SUITE_P(HashCollisionTestsFast, HashCollisionTests, testCasesFast, hashTestName);
+
+#ifdef NDEBUG
+INSTANTIATE_TEST_SUITE_P(HashCollisionTestsSlow, HashCollisionTests, testCasesSlow, hashTestName);
+#endif
 
 TEST(HashingTests, NullMoveEnPassantHashing) {
     const HashT withEnPassantTarget =
