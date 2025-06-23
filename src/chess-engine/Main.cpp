4bf752a6ca45bf4988a4cbdaae41b87f4abb616f
Joost Houben <j.b.houben@gmail.com>
2025-06-22 18:27:37 -0700

    Retune after rebase
    --------------------------------------------------
    Results of knight-on-outpost2 vs attackers-minus-defenders-factor (3+0.1, NULL, 512MB, UHO_2022_8mvs_big_+100_+129.pgn):
    Elo: 19.88 +/- 12.37, nElo: 27.49 +/- 17.07
    LOS: 99.92 %, DrawRatio: 35.80 %, PairsRatio: 1.26
    Games: 1592, Wins: 508, Losses: 417, Draws: 667, Points: 841.5 (52.86 %)
    Ptnml(0-2): [39, 187, 285, 214, 71], WL/DD Ratio: 1.14
    LLR: 2.96 (100.5%) (-2.94, 2.94) [0.00, 10.00]
    --------------------------------------------------
    SPRT ([0.00, 10.00]) completed - H1 was accepted
    Finished match

--- src/chess-engine/Main.cpp
+++ src/chess-engine/Main.cpp
@@ -23,7 +23,7 @@ int main() try {
 
         if (command == "uci") {
             Engine engine;
-            UciFrontEnd uciFrontEnd(engine, "knight-on-outpost");
+            UciFrontEnd uciFrontEnd(engine, "knight-on-outpost2");
             uciFrontEnd.run();
             break;
         } else if (command == "perft") {
