32fa284a7db4e41862f59cbc1c7fb6352ef52475
Joost Houben <j.b.houben@gmail.com>
2025-09-12 23:22:15 -0700

    Update uci name

--- src/chess-engine/Main.cpp
+++ src/chess-engine/Main.cpp
@@ -23,7 +23,7 @@ int main() try {
 
         if (command == "uci") {
             Engine engine;
-            UciFrontEnd uciFrontEnd(engine, "no-all-node-hash-move");
+            UciFrontEnd uciFrontEnd(engine, "mate-distance-pruning");
             uciFrontEnd.run();
             break;
         } else if (command == "perft") {
