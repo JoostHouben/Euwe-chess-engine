b2527e712da472f02cde6bbaed15729eb9070bfb
Joost Houben <JoostHouben@users.noreply.github.com>
2025-09-28 23:27:42 -0700

    Allow futility pruning when mate finding. (#55)
    Allow pruning of moves when side to move is looking to mate (but not when avoiding mate).
    
    Big improvement in matetrack performance:
    
    Before:
    ```
    Using C:\Git\Euwe-chess-engine\Archive\slightly-better-mdp.exe on .\matetrack.epd with --nodes 1000000
    Engine ID:     slightly-better-mdp
    Total FENs:    6554
    Found mates:   1447
    Best mates:    1219
    
    Using C:\Git\Euwe-chess-engine\Archive\slightly-better-mdp.exe on .\matedtrack.epd with --nodes 1000000
    Engine ID:     slightly-better-mdp
    Total FENs:    6536
    Found mates:   2199
    Best mates:    2008
    ```
    
    Now:
    ```
    Using C:\Git\Euwe-chess-engine\Archive\mate-futility-pruning.exe on .\matetrack.epd with --nodes 1000000
    Engine ID:     mate-futility-pruning
    Total FENs:    6554
    Found mates:   2077
    Best mates:    1618
    
    Using C:\Git\Euwe-chess-engine\Archive\mate-futility-pruning.exe on .\matedtrack.epd with --nodes 1000000
    Engine ID:     mate-futility-pruning
    Total FENs:    6536
    Found mates:   2707
    Best mates:    2360
    ```

--- src/chess-engine/Main.cpp
+++ src/chess-engine/Main.cpp
@@ -23,7 +23,7 @@ int main() try {
 
         if (command == "uci") {
             Engine engine;
-            UciFrontEnd uciFrontEnd(engine, "slightly-better-mdp");
+            UciFrontEnd uciFrontEnd(engine, "mate-futility-pruning");
             uciFrontEnd.run();
             break;
         } else if (command == "perft") {
