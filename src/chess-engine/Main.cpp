facf07b338a214e139c8ab161ee63247c8ed99a1
Joost Houben <j.b.houben@gmail.com>
2025-09-29 21:08:21 -0700

    Disable fp contraction to ensure consistency across compilers.

--- src/chess-engine/Main.cpp
+++ src/chess-engine/Main.cpp
@@ -23,7 +23,7 @@ int main() try {
 
         if (command == "uci") {
             Engine engine;
-            UciFrontEnd uciFrontEnd(engine, "mdp-equal");
+            UciFrontEnd uciFrontEnd(engine, "no-fp-contract");
             uciFrontEnd.run();
             break;
         } else if (command == "perft") {
